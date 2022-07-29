56e0efdba2cdbb3eb6d2fe518a501a2f10293f90
Quanvo2710 <109613117+Quanvo2710@users.noreply.github.com>
2022-07-29 23:09:40 +0700

    add name onl web

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("hello");
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+element.appendChild(para);
